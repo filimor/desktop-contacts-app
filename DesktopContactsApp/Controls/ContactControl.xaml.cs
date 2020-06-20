9fda6d9f6b69a523e2a0d602a7cb6e1d5c040b5c
Filipe Moreira <filimor@posteo.com.br>
2020-06-20 09:10:40 -0300

    Create User Control

---
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DesktopContactsApp.Classes;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopContactsApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact() { Name = "Name Lastname", Phone = "(11) 98765-4321", Email = "example@domain.com" }, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ContactControl;
+            if (control != null)
+            {
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+            }
+        }
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
